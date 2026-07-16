d9a9a8013a6ac44cde93137638e7e5d0f715b0da
ShankaragoudaRsasanur <shankargoudasasanur34@gmail.com>
2026-07-16 17:20:49 +0530

    first java project

---
+++ src/datatypes.java
@@ -0,0 +1,31 @@
+
+public class datatypes {
+
+	public static void main(String[] args) {
+//		interger type
+//		int myNumber = 10;
+//		System.out.println(myNumber);
+//	
+		
+//		string type
+//		String myLetter="shankar";
+//		System.out.println(myLetter);
+		
+//		boolean
+//		Boolean myBoolean=false;
+//		System.out.println(myBoolean);
+		
+//		float
+//		float doubleNumber = 8.888888888f;
+//		System.out.println(doubleNumber);
+//		
+		
+		int mySpeed =100;
+		System.out.println(mySpeed);
+				
+	}
+	
+
+}
+
+
