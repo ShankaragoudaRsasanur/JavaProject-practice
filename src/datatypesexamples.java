40563fadd734eee4277f4666c8edb95bedccd664
ShankaragoudaRsasanur <shankargoudasasanur34@gmail.com>
2026-07-17 18:05:23 +0530

    added all datatypes examples

---
+++ src/datatypesexamples.java
@@ -0,0 +1,30 @@
+
+public class datatypesexamples {
+
+	public static void main(String[] args) {
+		
+
+		        int age = 22;
+		        double salary = 25000.50;
+		        float height = 5.8f;
+		        char grade = 'A';
+		        boolean passed = true;
+		        String name = "Shankar";
+		        long mobile = 9876543210L;
+		        short marks = 450;
+		        byte number = 100;
+
+		        System.out.println("Name: " + name);
+		        System.out.println("Age: " + age);
+		        System.out.println("Salary: " + salary);
+		        System.out.println("Height: " + height);
+		        System.out.println("Grade: " + grade);
+		        System.out.println("Passed: " + passed);
+		        System.out.println("Mobile: " + mobile);
+		        System.out.println("Marks: " + marks);
+		        System.out.println("Number: " + number);
+		    }
+		
+	}
+
+
