768bf07bfabfd1bf163afd91544cba0e89c8f9c3
ShankaragoudaRsasanur <shankargoudasasanur34@gmail.com>
2026-07-22 17:39:23 +0530

    largest two number

---
+++ src/largestOftwonumber.java
@@ -0,0 +1,15 @@
+
+public class largestOftwonumber {
+
+	public static void main(String[] args) {
+		int a = 30;
+		int b = 20;
+		if(a>b) {
+		System.out.println(a + " is greater");}
+		else {
+			System.out.println(b + " is greater");
+		}
+
+	}
+
+}
