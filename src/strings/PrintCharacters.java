8ce6cfa6724cf3d26df96310ae32716112ec640d
ShankaragoudaRsasanur <shankargoudasasanur34@gmail.com>
2026-07-23 12:23:25 +0530

    task two

---
+++ src/strings/PrintCharacters.java
@@ -0,0 +1,17 @@
+package strings;
+
+public class PrintCharacters {
+
+    public static void main(String[] args) {
+
+        String name = "Java";
+
+        for (int i = 0; i < name.length(); i++) {
+
+            System.out.println(name.charAt(i));
+
+        }
+ 
+    }
+
+}
