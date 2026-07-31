c82114acf0bea03b14e043756bd89ab9491e7d5c
ShankaragoudaRsasanur <shankargoudasasanur34@gmail.com>
2026-07-31 18:50:56 +0530

    topic on encapsulation

--- src/constractor/overloadingConstructor.java
+++ src/constractor/overloadingConstructor.java
@@ -1,3 +1,13 @@
+/*
+* Constructor overloading is the process of 
+  creating multiple constructors in the same 
+  class with different parameter lists. 
+  It allows us to create objects in different ways 
+  depending on the available data.
+  */
+
+
+
 package constractor;
 
 public class overloadingConstructor {
---
+++ src/encapsulation/programTheory.java
@@ -0,0 +1,10 @@
+package encapsulation;
+
+public class programTheory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
